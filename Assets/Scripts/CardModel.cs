6a43c34ae3e8f727976e07693a6e90d858b202f6
kcanez <kevin.canez@gmail.com>
2016-06-16 20:56:52 -0700

    card game test

---
+++ Assets/Scripts/CardModel.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CardModel : MonoBehaviour 
+{
+	public Sprite[] faces;
+	public Sprite cardBack;
+
+
+	void Start () 
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	
+	}
+}
